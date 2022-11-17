d9bcb3130d9d09316f304858fca829306fcd7dba
Dominic Petruccelli <dpetruccelli15@gmail.com>
2022-11-17 12:19:44 -0500

    Dominic Code

---
+++ java/src/main/java/com/techelevator/models/Candy.java
@@ -0,0 +1,9 @@
+package com.techelevator.models;
+
+import java.math.BigDecimal;
+
+public class Candy extends Items{
+    public Candy(String slot, String name, BigDecimal price, String type) {
+        super(slot, name, price, type);
+    }
+}
---
+++ java/src/main/java/com/techelevator/models/Munchies.java
@@ -0,0 +1,9 @@
+package com.techelevator.models;
+
+import java.math.BigDecimal;
+
+public class Munchies extends Items{
+    public Munchies(String slot, String name, BigDecimal price, String type) {
+        super(slot, name, price, type);
+    }
+}
